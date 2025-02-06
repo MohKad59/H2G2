73df7bbdfbdf061c45b5dd20b23977eee7f7cec7
erwan lorand <erwan.lorand@protonmail.com>
2025-02-06 23:52:25 +0100

    API is back

--- data/dataArmes.js
+++ data/dataArmes.js
@@ -1,48 +1,98 @@
 const dataArmes = {
-  "armes": [
-    {
-      "id": 1,
-      "nom": "Épée longue",
-      "description": "Arme de chevalier par excellence, utilisée pour les combats en mêlée.",
-      "reference_film": "Godefroy de Montmirail manie une épée longue lors de ses batailles."
-    },
-    {
-      "id": 2,
-      "nom": "Masse d'armes",
-      "description": "Arme contondante efficace contre les armures, avec une tête en métal.",
-      "reference_film": "On aperçoit des gardes en portant dans certaines scènes."
-    },
-    {
-      "id": 3,
-      "nom": "Arbalète",
-      "description": "Arme de jet redoutable, permettant de percer les armures à distance.",
-      "reference_film": "Dans le film, un garde tire une arbalète sur Jacquouille qui s’enfuit."
-    },
-    {
-      "id": 4,
-      "nom": "Hallebarde",
-      "description": "Arme d'hast combinant une pique, une hache et un crochet.",
-      "reference_film": "Utilisée par les soldats du château dans certaines scènes."
-    },
-    {
-      "id": 5,
-      "nom": "Dague",
-      "description": "Arme de poing, souvent utilisée en dernier recours ou pour les assassinats.",
-      "reference_film": "Godefroy en possède une dans son équipement."
-    },
-    {
-      "id": 6,
-      "nom": "Arc long",
-      "description": "Arc puissant permettant de tirer des flèches à grande distance.",
-      "reference_film": "Peu présent dans *Les Visiteurs*, mais utilisé à l’époque médiévale."
-    },
-    {
-      "id": 7,
-      "nom": "Fléau d'armes",
-      "description": "Bâton avec une chaîne et une boule cloutée, redoutable en combat rapproché.",
-      "reference_film": "Non visible dans *Les Visiteurs*, mais typique des batailles médiévales."
-    }
-  ]
-}
+	armes: [
+		{
+			id: 1,
+			nom: "Épée longue",
+			description: "Une épée à deux mains utilisée par les chevaliers.",
+			url: "https://st3.depositphotos.com/1419868/35454/i/450/depositphotos_354540400-stock-photo-sword-disposed-diagonal-isolated-white.jpg",
+			Prix: "60 sous",
+		},
+		{
+			id: 2,
+			nom: "Hallebarde",
+			description:
+				"Une arme d'hast combinant une pique, un crochet et une hache.",
+			url: "https://st3.depositphotos.com/2361751/18265/i/600/depositphotos_182653712-stock-photo-ancient-halberd-or-war-ax.jpg",
+			Prix: "3 sous",
+		},
+		{
+			id: 3,
+			nom: "Arbalète",
+			description: "Une arme de tir puissante utilisée par les fantassins.",
+			url: "https://media.istockphoto.com/id/179070904/fr/photo/crossbow.jpg?b=1&s=612x612&w=0&k=20&c=zkt89IqbL_ZVM5zzc-jTUa2OVESJBh149gyKGdve-io=",
+			Prix: "40 sous",
+		},
+		{
+			id: 4,
+			nom: "Masse d'arme",
+			description:
+				"Une arme contondante utilisée pour écraser les armures ennemies.",
+			url: "https://www.antikcostume.com/images/1500/-26674.jpg",
+			Prix: "18 sous",
+		},
+		{
+			id: 5,
+			nom: "Dague",
+			description:
+				"Une petite lame rapide et discrète utilisée dans les combats rapprochés.",
+			url: "https://st3.depositphotos.com/2543399/15442/i/450/depositphotos_154420134-stock-photo-hunter-combat-hand-made-knife.jpg",
+			Prix: "14 sous",
+		},
+		{
+			id: 6,
+			nom: "Lance",
+			description:
+				"Une arme longue permettant d'attaquer les ennemis à distance de sûreté.",
+			url: "https://st5.depositphotos.com/17376970/76429/i/600/depositphotos_764291142-stock-photo-sharp-metal-tipped-spear-leans.jpg",
+			Prix: "25 sous",
+		},
+		{
+			id: 7,
+			nom: "Fléau d'armes",
+			description:
+				"Une boule cloutée attachée à une chaîne pour contourner les boucliers.",
+			url: "https://www.boutiquemedievale.fr/out/pictures/generated/product/1/720_960_80/0401042300.jpg",
+			Prix: "5 sous",
+		},
+		{
+			id: 8,
+			nom: "Arc long",
+			description:
+				"Un arc puissant utilisé par les archers anglais pour percer les armures.",
+			url: "https://cdn.leonardo.ai/users/77420abb-6002-47e4-907d-b2903db655a1/generations/2bf0d29f-4958-4bc1-ad31-299fcf9ea9ab/Leonardo_Phoenix_09_A_stunning_bokeh_effect_photography_of_a_m_2.jpg",
+			Prix: "22 sous",
+		},
+		{
+			id: 9,
+			nom: "Pique",
+			description:
+				"Une lance extrêmement longue utilisée par les fantassins contre la cavalerie.",
+			url: "https://st5.depositphotos.com/17376970/76429/i/600/depositphotos_764291142-stock-photo-sharp-metal-tipped-spear-leans.jpg",
+			Prix: "30 sous",
+		},
+		{
+			id: 10,
+			nom: "Francisque",
+			description: "Une hache de jet utilisée par les guerriers francs.",
+			url: "https://www.comptoirduchateau.fr/media/cache/image_produit_front/upload/catalogue/produit/TiBreiz/8f/170_1132294-20685-thickbox.jpeg",
+			Prix: "18 sous",
+		},
+		{
+			id: 11,
+			nom: "Katana",
+			description: "Une lame japonaise tranchante",
+			url: "https://st.depositphotos.com/1419868/1253/i/450/depositphotos_12531141-stock-photo-katana-sword.jpg",
+			Prix: "15 sous",
+		},
+		{
+			id: 12,
+			nom: "Guisarme",
+			description:
+				"Une arme d'hast médiévale avec une lame courbée pour attraper les ennemis.",
+			url: "https://www.antikcostume.com/images/540/-29308.jpg",
+			Prix: "20 sous",
+		},
+	],
+};
 
-export default dataArmes
+export default dataArmes;
